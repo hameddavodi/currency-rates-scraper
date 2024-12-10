dd7d80f18f4358a375d7a3f3429ff8fa94927e61
Hamed Davodi <hamed.davodi94@gmail.com>
2024-12-10 01:30:15 +0100

    Update currency_scraper.py

--- currency_scraper.py
+++ currency_scraper.py
@@ -59,6 +59,7 @@ class ValueExtractor:
             
             try:
                 # Navigate to the specified URL
+                await page.set_extra_http_headers({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"})
                 await page.goto(self.url, wait_until='networkidle')
                 
                 # Dictionary to store extracted values
